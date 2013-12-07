cec426edc6d50b8bb4a754b25d6b43f602b07b32
buurd <roland@javalia.se>
2013-12-07 07:42:07 +0100

    More tests

--- test/git_statistics/git_test.clj
+++ test/git_statistics/git_test.clj
@@ -16,12 +16,15 @@
         (is (= (:repo-dir git-statistics.git/repository) (git-statistics.git/get-repository-dir)))))
 
 (deftest switch-revision-test
-  (with-redefs [clj-jgit.porcelain/git-checkout (fn [repo revision] {:repo repo :revision revision})
-                git-statistics.git/get-revision-name (constantly "revisionname")]
+  (with-redefs [clj-jgit.porcelain/git-checkout (fn [repo revision] {:param1 repo :param2 revision})
+                git-statistics.git/get-revision-name (constantly "revisionname")
+                git-statistics.git/repository (constantly {:repo "a repository"})]
                 (let [value (switch-revision {:repo "mockrepo"})]
                   (is (= (git-statistics.git/get-revision-name "test") "revisionname"))
-                  (is (= (git-statistics.git/repository {:repo "mockrepo"})))
-                  (is (= (clj-jgit.porcelain/git-checkout "test1" "test2") {:repo "test1", :revision "test2"}))
-                  (is (= (:repo value) nil))
-                  (is (= (:revision value) "revisionname"))
+                  (is (= (git-statistics.git/repository {:repo "a repository"} {:repository "a repository"})))
+                  (is (= (clj-jgit.porcelain/git-checkout "test1" "test2") {:param1 "test1", :param2 "test2"}))
+                  (is (= (:param1 value) nil))
+                  (is (= (:param2 value) "revisionname"))
                   )))
+
+(run-tests)
